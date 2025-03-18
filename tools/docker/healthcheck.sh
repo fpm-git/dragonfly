3b82e4c04aedfa6458717024badce9c93783d19e
Jonathon Beauregard II <jonathon@floatplanemedia.com>
2025-03-18 09:31:04 -0700

    forgot that auth is a thing lol

--- tools/docker/healthcheck.sh
+++ tools/docker/healthcheck.sh
@@ -7,22 +7,29 @@ HOST="localhost"
 PORT=6379
 KEY="healthcheck_key_$(date +%s%N)"
 VALUE="ok"
-ROLE=$(redis-cli -h "$HOST" -p "$PORT" INFO REPLICATION | grep "role:" | cut -d":" -f2 | tr -d '\r')
+PASSWORD=${DFLY_requirepass:-""}
+
+REDIS_CLI_CMD="redis-cli -h $HOST -p $PORT"
+if [ -n "$PASSWORD" ]; then
+  REDIS_CLI_CMD="$REDIS_CLI_CMD -a $PASSWORD"
+fi
+
+ROLE=$($REDIS_CLI_CMD INFO REPLICATION | grep "role:" | cut -d":" -f2 | tr -d '\r')
 
 if [ "$ROLE" = "master" ]; then
   # Master pod, perform full read/write check
-  if [ "$(redis-cli -h $HOST -p $PORT SET $KEY $VALUE NX EX 5)" != "OK" ]; then
+  if [ "$($REDIS_CLI_CMD SET $KEY $VALUE NX EX 5)" != "OK" ]; then
     echo "Master pod healthcheck failed on SET operation."
     exit 1
   fi
 
-  if [ "$(redis-cli -h $HOST -p $PORT GET $KEY)" != "$VALUE" ]; then
+  if [ "$($REDIS_CLI_CMD GET $KEY)" != "$VALUE" ]; then
     echo "Master pod healthcheck failed on GET operation."
     exit 1
   fi
 else
   # Replica pod, perform basic ping check
-  if [ "$(redis-cli -h $HOST -p $PORT PING)" != "PONG" ]; then
+  if [ "$($REDIS_CLI_CMD PING)" != "PONG" ]; then
     echo "Replica pod healthcheck failed on PING operation."
     exit 1
   fi
