49a1185dab94ce8892e2fc67d784642b887fe023
Jonathon Beauregard II <jonathon@floatplanemedia.com>
2025-03-18 09:13:26 -0700

    improve health check

--- tools/docker/healthcheck.sh
+++ tools/docker/healthcheck.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 
+# Exit immediately on any command error (fail fast)
+set -e
+
 HOST="localhost"
-PORT=$HEALTHCHECK_PORT
+PORT=6379
+KEY="healthcheck_key_$(date +%s%N)"
+VALUE="ok"
+ROLE=$(redis-cli -h "$HOST" -p "$PORT" INFO REPLICATION | grep "role:" | cut -d":" -f2 | tr -d '\r')
 
-
-if [ -z "$HEALTHCHECK_PORT" ]; then
-  # try unpriveleged version first. This should cover cases when the container is running
-  # without root, for example:
-  # docker run  --group-add 999  --cap-drop=ALL --user 999 docker.dragonflydb.io/dragonflydb/dragonfly
-  DF_NET=$(netstat -tlnp | grep "1/dragonfly")
-  if [ -z "$DF_NET" ]; then
-    # if we failed, then lets try the priveleged version. is triggerred by the regular command:
-    # docker run docker.dragonflydb.io/dragonflydb/dragonfly
-    DF_NET=$(su dfly -c "netstat -tlnp" | grep "1/dragonfly")
+if [ "$ROLE" = "master" ]; then
+  # Master pod, perform full read/write check
+  if [ "$(redis-cli -h $HOST -p $PORT SET $KEY $VALUE NX EX 5)" != "OK" ]; then
+    echo "Master pod healthcheck failed on SET operation."
+    exit 1
   fi
 
-  # check all the TCP ports, and fetch the port.
-  # For cases when dragonfly opens multiple ports, we filter with tail to choose one of them.
-  PORT=$(echo $DF_NET | grep -oE ':[0-9]+' | cut -c2- | tail -n 1)
+  if [ "$(redis-cli -h $HOST -p $PORT GET $KEY)" != "$VALUE" ]; then
+    echo "Master pod healthcheck failed on GET operation."
+    exit 1
+  fi
+else
+  # Replica pod, perform basic ping check
+  if [ "$(redis-cli -h $HOST -p $PORT PING)" != "PONG" ]; then
+    echo "Replica pod healthcheck failed on PING operation."
+    exit 1
+  fi
 fi
 
-_healthcheck="nc -q1 $HOST $PORT"
-
-echo PING | ${_healthcheck}
-
-exit $?
+exit 0
